5fcd1d032193ea56f6115fdc9d0d8eaafab8aa18
zaafra06 <pzg120106@gmail.com>
2025-09-18 18:37:04 +0200

    subo todo

---
+++ Workspace estructura de datos/Ejemplo03/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int x = 'a';
+	x=x+1;
+	char c=x;
+	cout << "El valor de int \"x\" es " << x << "." << endl;
+	cout << "El valor de char \"c\" es " << c << "." << endl;
+	
+	return 0;
+}
